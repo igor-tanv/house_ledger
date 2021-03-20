f55993f77acb2a6741e857e98209495bf12dc3c2
Igor Tatarinov <igor.tanv@gmail.com>
2021-03-20 17:55:04 +0700

    new ledger entry form added

--- client/src/components/ledger-entry/index.js
+++ client/src/components/ledger-entry/index.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDropdown from "react-dropdown"
+import 'react-dropdown/style.css';
+
+import { toValueLabel } from '../../modules/object'
+
+import users from '../../data/users/users.json'
+
+
+const defaultValues = {
+  user: '',
+  item: '',
+  cost: ''
+}
 
 export default function LedgerEntry() {
+  const [values, setValues] = useState(defaultValues)
+
+  const updateCost = (e) => {
+    let cost = parseInt(e.target.value);
+    setValues((prev) => ({
+      ...prev,
+      cost,
+    }))
+  };
+
+  const updateUser = (e) => {
+    let user = e.value
+    setValues((prev) => ({
+      ...prev,
+      user,
+    }))
+  };
+
+  const updateItem = (e) => {
+    let item = e.target.value
+    setValues((prev) => ({
+      ...prev,
+      item,
+    }))
+  };
+
   return (
     <div>
-      <input
-        type="number"
-        min="0"
-      />
+      <form>
+        <ReactDropdown
+          options={toValueLabel(users)}
+          onChange={updateUser}
+          placeholder="Select a user"
+        />
+        <input
+          type="number"
+          min="0"
+          onChange={updateCost}
+        />
+        <textarea
+          onChange={updateItem}
+        />
+      </form>
     </div>
   )
 }
