c20fac51512c3b5e2e5720e98b71db29a77588da
Igor Tatarinov <igor.tanv@gmail.com>
2021-03-23 16:42:01 +0700

    sort ledger entries by date on server

--- server/src/routes/ledger/index.js
+++ server/src/routes/ledger/index.js
@@ -4,7 +4,8 @@ const LedgerService = require('../../services/index')
 const router = new express.Router()
 
 router.get('/api', async (req, res) => {
-  res.status(200).json(await LedgerService.read())
+  const ledger = await LedgerService.read()
+  res.status(200).json(ledger.sort((a, b) => b.timestamp - a.timestamp))
 })
 
 router.post('/api/ledger', async (req, res) => {
