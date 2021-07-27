632abb25d1a0a4114f7268dfed015d29574450aa
Igor Tatarinov <igor.tanv@gmail.com>
2021-07-27 13:57:33 +0700

    short ledger copied from long ledger successfully

--- client/src/components/ledger-balance/index.js
+++ client/src/components/ledger-balance/index.js
@@ -23,6 +23,8 @@ function calculateBalance(ledger, ledgerUsers) {
 
 function calculateShortLedgerBalance(ledger) {
 
+  console.log(ledger, 26)
+
   const { transactions } = ledger
 
   const userArray = ledger[0].users.split(' ')
--- client/src/pages/short-term-ledger/index.js
+++ client/src/pages/short-term-ledger/index.js
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom"
 
 import LedgerEntry from '../../components/ledger-entry'
+import LedgerTable from '../../components/ledger-table'
 import LedgerBalance from '../../components/ledger-balance'
 
 import { apiFetch } from '../../modules/api-fetch'
-import HomeButton from '../../ui/home-button'
+import { usersToObject } from '../../modules/users-to-object'
 
 import { defaultValues } from '../../form-helpers/defaultEntryValues'
 
 function ShortTermLedger({ match }) {
   const [values, setValues] = useState(defaultValues)
-  const [ledger, setLedger] = useState({})
   const [users, setUsers] = useState('')
+  const [ledger, setLedger] = useState([])
   const [error, setError] = useState('')
+
 
   useEffect(() => {
     apiFetch(`ledger/short/${match.params.id}`).then((json) => {
-      //destructure ledger obj and transactions arr
-      let { ledger, transactions } = json
-      ledger.transactions = transactions
+      const { ledger, transactions } = json
       setUsers(ledger.users)
-      setLedger(ledger)
+      setLedger(transactions)
+      //setLedger(json)
     })
+
   }, [])
 
   function handleSubmit(e) {
@@ -30,19 +32,29 @@ function ShortTermLedger({ match }) {
     apiFetch(`ledger/short/${match.params.id}`, 'post', values)
       .then((json) => {
         setValues(defaultValues)
-        let { ledger, transactions } = json
-        ledger.transactions = transactions
-        setLedger(ledger)
       })
       .catch((error) => {
-        console.log(error)
         setError(error)
       });
   }
 
+  function clearActiveLedger(e) {
+    e.preventDefault();
+    apiFetch(`ledger/short/clear/${match.params.id}`, 'post')
+      .then((json) => {
+        setLedger(json)
+      })
+      .catch((error) => {
+        setError(error)
+      });
+  }
+
+  const title = Object.values(usersToObject(users)).reduce((acc, cur) => acc += ' ' + cur, ' ')
+
   return <div className="container-wrapper">
+    <h1>Short Ledger For: {title}</h1>
     {error && <span className="error">{error}</span>}
-    <HomeButton />
+    <button onClick={clearActiveLedger}>Clear This Ledger</button>
     <div className="ledger-entry">
       <LedgerEntry
         values={values}
@@ -53,9 +65,8 @@ function ShortTermLedger({ match }) {
     </div>
     {ledger.length > 0 ? (
       <div>
-        <LedgerBalance
-          props={ledger}
-          users={users} />
+        <LedgerBalance props={ledger} users={users} />
+        <LedgerTable props={ledger} />
       </div>
     ) : (
       <div>No Ledger Entries</div>
